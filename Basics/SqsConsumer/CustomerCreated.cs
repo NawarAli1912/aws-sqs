becbf725d08ed600e8b93c8b0c51999f9ce54c04
Nawar <111868983+NawarAli1912@users.noreply.github.com>
2023-03-16 14:05:27 +0300

    initalizing-the-basic-message-consumer

---
+++ Basics/SqsConsumer/CustomerCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace SqsConsumer;
+
+public class CustomerCreated
+{
+    public required Guid Id { get; set; }
+
+    public required string FullName { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string GitHubUsername { get; set; }
+
+    public required DateTime DateOfBirth { get; set; }
+}
---
+++ Basics/SqsConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+
+var sqsClient = new AmazonSQSClient();
+
+var queueRrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueRrlResponse.QueueUrl,
+
+    // the message attribute is not loaded for 
+    // efficiencies reasons you have to explicitly request them
+    AttributeNames = new List<string> { "All" },
+    MessageAttributeNames = new List<string> { "All" }
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id : {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        // Consuming a message dosen't mean that it's been deleted by the consumer.
+        await sqsClient.DeleteMessageAsync(queueRrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(3000);
+}
