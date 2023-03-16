9a67836d6a3657a995956aa22ca2d5bb0771ce7b
Nawar <111868983+NawarAli1912@users.noreply.github.com>
2023-03-16 05:19:29 +0300

    initalizing-the-api-and-basic-sqs-publisher

---
+++ Basics/SqsPublisher/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "nr.ali1912@gmail.com",
+    FullName = "Nawar Ali",
+    GitHubUsername = "NawarAli1912",
+    DateOfBirth = new DateTime(1998, 12, 19)
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new()
+    {
+        {
+            "MessageType",
+            new()
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response);
